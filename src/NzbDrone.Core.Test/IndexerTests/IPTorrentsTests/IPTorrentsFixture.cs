Sonarr/Sonarr

New: Implemented Torrent Download Clients: uTorrent, Transmission and Deluge. And several public and private Torrent Indexers.

src/NzbDrone.Core.Test/IndexerTests/IPTorrentsTests/IPTorrentsFixture.cs
@@ -1,0 +1,69 @@
+﻿using Moq;
+using NUnit.Framework;
+using NzbDrone.Common;
+using NzbDrone.Common.Http;
+using NzbDrone.Core.Indexers;
+using NzbDrone.Core.Indexers.IPTorrents;
+using NzbDrone.Core.Parser.Model;
+using NzbDrone.Core.Test.Framework;
+using NzbDrone.Core.ThingiProvider;
+using NzbDrone.Test.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+
+namespace NzbDrone.Core.Test.IndexerTests.IPTorrentsTests
+{
+    [TestFixture]
+    public class IPTorrentsFixture : CoreTest<IPTorrents>
+    {
+        private String _recentFeed;
+
+        [TestFixtureSetUp]
+        public void SetupFixture()
+        {
+            Subject.Definition = new IndexerDefinition()
+                                    {
+                                        Name = "IPTorrents",
+                                        Settings = new IPTorrentsSettings()
+                                    };
+
+            _recentFeed = ReadAllText(@"Files/RSS/IPTorrents.xml");
+        }
+
+        [Test]
+        public void Indexer_TestFeedParser_IPTorrents()
+        {
+            var httpProviderMock = Mocker.GetMock<IHttpProvider>();
+            httpProviderMock.Setup(o => o.DownloadString(It.IsAny<String>()))
+                            .Returns(_recentFeed);
+
+            var httpProvider = httpProviderMock.Object;
+
+            var fetchService = new FetchFeedService(httpProvider, TestLogger);
+            var releases = fetchService.FetchRss(Subject);
+
+            releases.Should().HaveCount(5);
+
+            var firstRelease = releases.First();
+
+            Assert.IsInstanceOf<TorrentInfo>(firstRelease);
+
+            var torrentInfo = (TorrentInfo)firstRelease;
+
+            torrentInfo.Title.Should().Be("24 S03E12 720p WEBRip h264-DRAWER");
+            torrentInfo.DownloadProtocol.Should().Be(DownloadProtocol.Torrent);
+            torrentInfo.DownloadUrl.Should().Be("http://iptorrents.com/download.php/1234/24.S03E12.720p.WEBRip.h264-DRAWER.torrent?torrent_pass=abcd");
+            torrentInfo.Indexer.Should().Be(Subject.Definition.Name);
+            firstRelease.PublishDate.Should().Be(DateTime.Parse("2014/05/12 19:06:34"));
+            torrentInfo.Size.Should().Be(1471026299);
+            torrentInfo.InfoHash.Should().Be(null);
+            torrentInfo.MagnetUrl.Should().Be(null);
+            torrentInfo.Peers.Should().Be(null);
+            torrentInfo.Seeds.Should().Be(null);
+        }
+    }
+}

src/NzbDrone.Core/Download/Clients/Deluge/DelugeTorrent.cs
@@ -1,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NzbDrone.Core.Download.Clients.Deluge
+{
+    public class DelugeTorrent
+    {
+        public String Hash { get; set; }
+        public String Name { get; set; }
+        public String State { get; set; }
+        public Double Progress { get; set; }
+        public Int32 Eta { get; set; }
+        public String Message { get; set; }
+
+        [JsonProperty(PropertyName = "is_finished")]
+        public Boolean IsFinished { get; set; }
+        
+        // TODO: Also 'move_on_completed_path'. What is the difference?
+        [JsonProperty(PropertyName = "move_completed_path")] 
+        public String DownloadPath { get; set; }
+
+        [JsonProperty(PropertyName = "total_size")]
+        public Int64 Size { get; set; }
+
+        [JsonProperty(PropertyName = "total_done")]
+        public Int64 BytesDownloaded { get; set; }
+
+        [JsonProperty(PropertyName = "time_added")]
+        public Double DateAdded { get; set; }
+
+        [JsonProperty(PropertyName = "active_time")]
+        public Int32 SecondsDownloading { get; set; }
+    }
+}

src/NzbDrone.Core/Indexers/BitMeTv/BitMeTv.cs
@@ -1,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using NzbDrone.Core.ThingiProvider;
+using FluentValidation.Results;
+using System.Linq;
+
+namespace NzbDrone.Core.Indexers.BitMeTv
+{
+    public class BitMeTv : IndexerBase<BitMeTvSettings>
+    {
+        public override DownloadProtocol Protocol
+        {
+            get
+            {
+                return DownloadProtocol.Torrent;
+            }
+        }
+
+        public override Int32 SupportedPageSize
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        public override Boolean SupportsSearching
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public override IParseFeed Parser
+        {
+            get
+            {
+                return new BitMeTvRssParser();
+            }
+        }
+
+        public override IEnumerable<String> RecentFeed
+        {
+            get
+            {
+                yield return CreateRssUrl(String.Format("rss.php?uid={0}&passkey={1}", Settings.UserId, Settings.RssPasskey));
+            }
+        }
+
+        public override IEnumerable<String> GetEpisodeSearchUrls(List<String> titles, Int32 tvRageId, Int32 seasonNumber, Int32 episodeNumber)
+        {
+            return Enumerable.Empty<String>();
+        }
+
+        public override IEnumerable<String> GetDailyEpisodeSearchUrls(List<String> titles, Int32 tvRageId, DateTime date)
+        {
+            return Enumerable.Empty<String>();
+        }
+
+        public override IEnumerable<String> GetAnimeEpisodeSearchUrls(List<String> titles, Int32 tvRageId, Int32 absoluteEpisodeNumber)
+        {
+            return Enumerable.Empty<String>();
+        }
+
+        public override IEnumerable<String> GetSeasonSearchUrls(List<String> titles, Int32 tvRageId, Int32 seasonNumber, Int32 offset)
+        {
+            return Enumerable.Empty<String>();
+        }
+
+        public override IEnumerable<String> GetSearchUrls(String query, Int32 offset)
+        {
+            return Enumerable.Empty<String>();
+        }
+
+        private String CreateRssUrl(String relativeUrl)
+        {
+            return String.Format("{0}/{1}", Settings.BaseUrl.Trim().TrimEnd('/'), relativeUrl);
+        }
+
+        public override ValidationResult Test()
+        {
+            return new ValidationResult();
+        }
+    }
+}

mythjuha/NzbDrone

src/NzbDrone.Core.Test/IndexerTests/IPTorrentsTests/IPTorrentsFixture.cs
@@ -1,0 +1,69 @@
+﻿using Moq;
+using NUnit.Framework;
+using NzbDrone.Common;
+using NzbDrone.Common.Http;
+using NzbDrone.Core.Indexers;
+using NzbDrone.Core.Indexers.IPTorrents;
+using NzbDrone.Core.Parser.Model;
+using NzbDrone.Core.Test.Framework;
+using NzbDrone.Core.ThingiProvider;
+using NzbDrone.Test.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+
+namespace NzbDrone.Core.Test.IndexerTests.IPTorrentsTests
+{
+    [TestFixture]
+    public class IPTorrentsFixture : CoreTest<IPTorrents>
+    {
+        private String _recentFeed;
+
+        [TestFixtureSetUp]
+        public void SetupFixture()
+        {
+            Subject.Definition = new IndexerDefinition()
+                                    {
+                                        Name = "IPTorrents",
+                                        Settings = new IPTorrentsSettings() {  Url = "http://fake.com/" }
+                                    };
+
+            _recentFeed = ReadAllText(@"Files/RSS/IPTorrents.xml");
+        }
+
+        [Test]
+        public void Indexer_TestFeedParser_IPTorrents()
+        {
+            var httpClientMock = Mocker.GetMock<IHttpClient>();
+            httpClientMock.Setup(o => o.Get(It.IsAny<HttpRequest>()))
+                          .Returns<HttpRequest>(r => new HttpResponse(r, new HttpHeader(), _recentFeed));
+
+            var httpClient = httpClientMock.Object;
+
+            var fetchService = new FetchFeedService(httpClient, TestLogger);
+            var releases = fetchService.FetchRss(Subject);
+
+            releases.Should().HaveCount(5);
+
+            var firstRelease = releases.First();
+
+            Assert.IsInstanceOf<TorrentInfo>(firstRelease);
+
+            var torrentInfo = (TorrentInfo)firstRelease;
+
+            torrentInfo.Title.Should().Be("24 S03E12 720p WEBRip h264-DRAWER");
+            torrentInfo.DownloadProtocol.Should().Be(DownloadProtocol.Torrent);
+            torrentInfo.DownloadUrl.Should().Be("http://iptorrents.com/download.php/1234/24.S03E12.720p.WEBRip.h264-DRAWER.torrent?torrent_pass=abcd");
+            torrentInfo.Indexer.Should().Be(Subject.Definition.Name);
+            firstRelease.PublishDate.Should().Be(DateTime.Parse("2014/05/12 19:06:34"));
+            torrentInfo.Size.Should().Be(1471026299);
+            torrentInfo.InfoHash.Should().Be(null);
+            torrentInfo.MagnetUrl.Should().Be(null);
+            torrentInfo.Peers.Should().Be(null);
+            torrentInfo.Seeds.Should().Be(null);
+        }
+    }
+}

src/NzbDrone.Core/Download/Clients/Deluge/DelugeTorrent.cs
@@ -1,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NzbDrone.Core.Download.Clients.Deluge
+{
+    public class DelugeTorrent
+    {
+        public String Hash { get; set; }
+        public String Name { get; set; }
+        public String State { get; set; }
+        public Double Progress { get; set; }
+        public Double Eta { get; set; }
+        public String Message { get; set; }
+
+        [JsonProperty(PropertyName = "is_finished")]
+        public Boolean IsFinished { get; set; }
+        
+        // Other paths: What is the difference between  'move_completed_path' and 'move_on_completed_path'?
+        /*
+        [JsonProperty(PropertyName = "move_completed_path")] 
+        public String DownloadPathMoveCompleted { get; set; }
+        [JsonProperty(PropertyName = "move_on_completed_path")] 
+        public String DownloadPathMoveOnCompleted { get; set; }
+        */
+
+        [JsonProperty(PropertyName = "save_path")] 
+        public String DownloadPath { get; set; }
+
+        [JsonProperty(PropertyName = "total_size")]
+        public Int64 Size { get; set; }
+
+        [JsonProperty(PropertyName = "total_done")]
+        public Int64 BytesDownloaded { get; set; }
+
+        [JsonProperty(PropertyName = "time_added")]
+        public Double DateAdded { get; set; }
+
+        [JsonProperty(PropertyName = "active_time")]
+        public Int32 SecondsDownloading { get; set; }
+    }
+}

src/NzbDrone.Core/Indexers/BitMeTv/BitMeTv.cs
@@ -1,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+using NzbDrone.Core.ThingiProvider;
+using FluentValidation.Results;
+using System.Linq;
+
+namespace NzbDrone.Core.Indexers.BitMeTv
+{
+    public class BitMeTv : IndexerBase<BitMeTvSettings>
+    {
+        public override DownloadProtocol Protocol
+        {
+            get
+            {
+                return DownloadProtocol.Torrent;
+            }
+        }
+
+        public override Int32 SupportedPageSize
+        {
+            get
+            {
+                return 0;
+            }
+        }
+
+        public override Boolean SupportsSearch
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public override IParseFeed Parser
+        {
+            get
+            {
+                return new BitMeTvRssParser();
+            }
+        }
+
+        public override IEnumerable<String> RecentFeed
+        {
+            get
+            {
+                yield return CreateRssUrl(String.Format("rss.php?uid={0}&passkey={1}", Settings.UserId, Settings.RssPasskey));
+            }
+        }
+
+        public override IEnumerable<String> GetEpisodeSearchUrls(List<String> titles, Int32 tvRageId, Int32 seasonNumber, Int32 episodeNumber)
+        {
+            return Enumerable.Empty<String>();
+        }
+
+        public override IEnumerable<String> GetDailyEpisodeSearchUrls(List<String> titles, Int32 tvRageId, DateTime date)
+        {
+            return Enumerable.Empty<String>();
+        }
+
+        public override IEnumerable<String> GetAnimeEpisodeSearchUrls(List<String> titles, Int32 tvRageId, Int32 absoluteEpisodeNumber)
+        {
+            return Enumerable.Empty<String>();
+        }
+
+        public override IEnumerable<String> GetSeasonSearchUrls(List<String> titles, Int32 tvRageId, Int32 seasonNumber, Int32 offset)
+        {
+            return Enumerable.Empty<String>();
+        }
+
+        public override IEnumerable<String> GetSearchUrls(String query, Int32 offset)
+        {
+            return Enumerable.Empty<String>();
+        }
+
+        private String CreateRssUrl(String relativeUrl)
+        {
+            return String.Format("{0}/{1}", Settings.BaseUrl.Trim().TrimEnd('/'), relativeUrl);
+        }
+
+        public override ValidationResult Test()
+        {
+            return new ValidationResult();
+        }
+    }
+}